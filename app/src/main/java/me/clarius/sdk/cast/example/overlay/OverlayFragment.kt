1b0fef40f400e3f7e49a96b338ac37756ec27d65
Adam Simard <48142435+Simard302@users.noreply.github.com>
2025-03-03 13:50:56 -0500

    Added button events

--- app/src/main/java/me/clarius/sdk/cast/example/overlay/OverlayFragment.kt
+++ app/src/main/java/me/clarius/sdk/cast/example/overlay/OverlayFragment.kt
@@ -2,6 +2,7 @@ package me.clarius.sdk.cast.example.overlay
 
 import android.content.ComponentName
 import android.content.ServiceConnection
+import android.content.res.ColorStateList
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
@@ -13,10 +14,18 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import me.clarius.sdk.Cast
 import me.clarius.sdk.ProbeInfo
+import me.clarius.sdk.cast.example.R
 import me.clarius.sdk.cast.example.clarius.CastService
 import me.clarius.sdk.cast.example.clarius.CastService.CastBinder
 import me.clarius.sdk.cast.example.databinding.FragmentOverlayBinding
@@ -45,7 +54,126 @@ class OverlayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentOverlayBinding.inflate(inflater, container, false)
+
         return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val directionButton: Button = view.findViewById(R.id.desiredInsertDirection)
+
+        directionButton.setOnClickListener {
+            if (directionButton.text == "Insertion Side:\nLeft") {
+
+                directionButton.text = "Insertion Side:\nRight"
+            } else {
+                directionButton.text = "Insertion Side:\nLeft"
+            }
+        }
+
+        val needleButton: LinearLayout = view.findViewById(R.id.btnNeedle)
+        val needleVisibility: ImageView = view.findViewById(R.id.needleVisibility)
+        val showDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.show)
+
+        needleButton.setOnClickListener {
+            val visibilityDrawable = needleVisibility.drawable;
+            if (visibilityDrawable != null && showDrawable != null) {
+                if (visibilityDrawable.constantState == showDrawable.constantState) {
+                    needleVisibility.setImageResource(R.drawable.hide)
+                    needleButton.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.purple
+                        )
+                    )
+                } else {
+                    needleVisibility.setImageResource(R.drawable.show)
+                    needleButton.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.white
+                        )
+                    )
+                }
+            }
+
+        }
+
+        val nerveButton: LinearLayout = view.findViewById(R.id.btnNerve)
+        val nerveVisibility: ImageView = view.findViewById(R.id.nerveVisibility)
+        val hideDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.hide)
+
+        nerveButton.setOnClickListener {
+            val visibilityDrawable = nerveVisibility.drawable;
+            if (visibilityDrawable != null && hideDrawable != null) {
+                if (visibilityDrawable.constantState == hideDrawable.constantState) {
+                    nerveVisibility.setImageResource(R.drawable.show)
+                    nerveButton.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.purple
+                        )
+                    )
+                } else {
+                    nerveVisibility.setImageResource(R.drawable.hide)
+                    nerveButton.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.white
+                        )
+                    )
+                }
+
+            }
+        }
+
+        val safeZoneSlider: SeekBar = view.findViewById(R.id.seekBarSafeZone)
+        val safeZoneValueText: TextView = view.findViewById(R.id.tvSafeZoneValue)
+
+        safeZoneSlider.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                safeZoneValueText.text = "$progress x"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?){
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+
+        val gainSlider: SeekBar = view.findViewById(R.id.seekBarGain)
+        val gainValueText: TextView = view.findViewById(R.id.tvGainValue)
+
+        gainSlider.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val gain = progress.toDouble() / 2
+                gainValueText.text = "$gain x"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?){
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+
+        val freqSlider: SeekBar = view.findViewById(R.id.seekBarFrequency)
+        val freqTextValue: TextView = view.findViewById(R.id.tvFrequencyValue)
+
+        freqSlider.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val freq = progress.toDouble() / 2
+                freqTextValue.text = "$freq MHz"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?){
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
     }
 
     private val castConnection: ServiceConnection = object : ServiceConnection {
@@ -61,30 +189,32 @@ class OverlayFragment : Fragment() {
 
 //            model!!.setNewModel(procedure)
 
-
-            // Observe the processed image LiveData
+//
+//            // Observe the processed image LiveData
             castBinder!!.getProcessedImage().observe(
                 viewLifecycleOwner
             ) { processedImage: Bitmap ->
                 // Apply your image manipulation logic here
                 val manipulatedImage = model!!.process(processedImage)
-                binding!!.imageView.setImageBitmap(manipulatedImage)
+                binding!!.ultrasoundImage.setImageBitmap(manipulatedImage)
             }
-
+//
             Log.d(TAG, "model created")
-
+//
             castBinder!!.timestamp.observe(
                 viewLifecycleOwner
             ) { timestamp: Long? -> this@OverlayFragment.setTimestamp(timestamp) }
+
             castBinder!!.getError().observe(
                 viewLifecycleOwner
             ) { text: String? -> this@OverlayFragment.showError(text) }
-            castBinder!!.getRawDataProgress().observe(
-                viewLifecycleOwner
-            ) { progress: Int? ->
-                binding!!.rawDataDownloadProgressBar.progress =
-                    progress!!
-            }
+
+//            castBinder!!.getRawDataProgress().observe(
+//                viewLifecycleOwner
+//            ) { progress: Int? ->
+//                binding!!.rawDataDownloadProgressBar.progress =
+//                    progress!!
+//            }
         }
 
         override fun onServiceDisconnected(component: ComponentName) {
